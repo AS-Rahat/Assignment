32ba45f4da652b463cbbff80428b04b7aa0f96e3
AS-Rahat <absayed807@gmail.com>
2024-03-16 12:25:57 +0600

    First commit

---
+++ src/Excercise2.java
@@ -0,0 +1,9 @@
+public class Excercise2 {
+    public static void main(String arg[]){
+         double x =3;
+         double y =4;
+      System.out.println(3*2);
+      System.out.println(3*3);
+     System.out.println(Math.pow(x,y));
+    }
+}
---
+++ src/Excercise3.java
@@ -0,0 +1,15 @@
+public class Excercise3 {
+    public static void main(String arg[]){
+        int x = 5;
+       int y = 6;
+      System.out.println("The sum is =" + Integer.sum(x, y));
+      System.out.println("The diffrence is =" + Math.subtractExact(x, y));
+      System.out.println("The product is =" + x*y);
+      System.out.println("The average is =" + (x+y)/2);
+      System.out.println("The maximum is =" + Integer.max(x,y));
+      System.out.println("THe minimum is =" + Integer.min(x,y));
+
+
+
+    }
+}
